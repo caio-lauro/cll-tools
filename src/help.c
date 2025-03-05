06d607bfe08014843c03d5e49aa228ba79221138
caio-lauro <caiolaurodelima@gmail.com>
2025-03-05 09:39:36 -0300

    Add help.c

---
+++ src/help.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "../include/help.h"
+#include "../include/colors.h"
+
+void printCommandsHelp();
+
+int getHelp() {
+	printf(BOLD "Usage:" 
+		NORMAL "\n\tcll-tools [OPTION] [ARGS]\n");
+
+	printf(BOLD "Description:" 
+		NORMAL "\n\tcll-tools is a program designed and mantained by Caio Lauro to provide assistance in various tasks he does.\n");
+
+	printf(BOLD "Software Commands:" NORMAL "\n");
+	printCommandsHelp();
+
+	return 0;
+}
+
+void printCommandsHelp() {
+	printf("\t-h, --help\t\t");
+	printf("Provides help for the user.\n");
+
+	printf("\tupdate-system\t\t");
+	printf("Updates the sytem when needed, use with sudo.\n");
+}
