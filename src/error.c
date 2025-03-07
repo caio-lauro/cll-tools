e0166da576c60bb5fca536fedb53dc9b2a8e48ed
caio-lauro <caiolaurodelima@gmail.com>
2025-03-07 09:46:23 -0300

    feat: add error code to simplify error treatment

---
+++ src/error.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "../include/error.h"
+
+int commandNotFound() {
+	printf("Command not found. Use --help or -h for help.\n");
+	return 1;
+}
+
+int wrongUsage() {
+	printf("Wrong usage. Use --help or -h for help.\n");
+	return 2;
+}
+
+int throwError(int errorCode) {
+	switch (errorCode) {
+		case 1:
+			return commandNotFound();
+		case 2:
+			return wrongUsage();
+		default:
+			printf("Misusage of throwError, error code not found.\n");
+			return -1;
+	}
+}
